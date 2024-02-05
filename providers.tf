50aa4127832dad4ea02293fa1edc31347e30573e
MKTHEPLUGG <michielvh@outlook.com>
2024-02-05 01:32:48 +0100

    removing aliasses, no longer needed

--- providers.tf
+++ providers.tf
@@ -3,12 +3,10 @@ terraform {
     azurerm = {
       source                = "hashicorp/azurerm"
       version               = ">=3.0.0,<4.0.0"
-      configuration_aliases = [azurerm.mgmt]
     }
     azapi = {
       source = "Azure/azapi"
       version = ">=1.0.0,<2.0.0"
-      configuration_aliases = [azurerm.mgmt]
     }
   }
 }
